012cdeab5bf0f25190660b58c44793e6ffdd7f2b
Alvyn Wira Pratama <168216543+alvynpratama@users.noreply.github.com>
2025-04-11 18:08:58 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AuthController;
 
-// Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
-// Protected routes (gunakan JWT middleware)
 Route::middleware(['jwt.auth'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me',      [AuthController::class, 'me']);
     
-    // Resource Student (semua method: index, show, store, update, destroy)
     Route::apiResource('/students', StudentController::class);
 });
