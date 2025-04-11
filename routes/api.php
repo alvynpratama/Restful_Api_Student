a2faea49bad9aeb226106d94e1cf1566eb8e0a51
Alvyn Wira Pratama <168216543+alvynpratama@users.noreply.github.com>
2025-04-11 18:10:48 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AuthController;
 
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
+
 
 Route::middleware(['jwt.auth'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me',      [AuthController::class, 'me']);
-    
+
+
     Route::apiResource('/students', StudentController::class);
 });
